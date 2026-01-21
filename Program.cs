a3b6d2925ccc1910a9dd0fba307477223b99b3d2
lukehoop <166653189+lukehoop@users.noreply.github.com>
2026-01-21 09:14:11 -0700

    save 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+while (true);
+{
+    string user_selection = "";
+    Console.WriteLine("Please select an option from the menu:");
+    Console.WriteLine("1. Add Food Items");
+    Console.WriteLine("2. Delete Food Items");
+    Console.WriteLine("3. Print List of Current Food Items");
+    Console.WriteLine("4. Exit the Program");
+    user_selection = Console.ReadLine();
+
+    if (user_selection == "1");
+    {
+
+    }
+    if (user_selection == "2");
+    {
+
+    }
+    if (user_selection == "3");
+    {
+
+    }
+    if (user_selection == "4");
+    {
+        break
+    }
+}
